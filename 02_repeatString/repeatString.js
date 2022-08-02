f84a3873d7310a555f448959143a16977fa3db2c
Gautier DAVID <gautier.d.91@gmail.com>
2022-08-02 20:55:56 +0200

    first two exercises done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,7 @@
-const repeatString = function() {
 
+// default value parameter is a random between 0 and 999 in case there is no num paramater added
+const repeatString = function(string, num = Math.floor(Math.random()) * 1000) {
+    return num < 0 ? "ERROR" : string.repeat(num)
 };
 
 // Do not edit below this line
