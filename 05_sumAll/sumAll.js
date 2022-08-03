24d94298719c3ab9f02e58f0e40441fe79441f84
Gautier DAVID <gautier.d.91@gmail.com>
2022-08-03 15:28:52 +0200

    all test for the fifth exercises are cleared

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+    
+    for(const arg of args) {
+        if(arg < 0 || typeof arg !== 'number') return "ERROR"
+    }
 
-};
+    let total = 0;
+    for(let i = Math.min(...args); i <= Math.max(...args); i++) {
+      total += i
+    }
+    return total
+  };
 
 // Do not edit below this line
 module.exports = sumAll;
