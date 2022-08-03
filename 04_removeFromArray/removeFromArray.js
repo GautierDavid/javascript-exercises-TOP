304a51e79c4dc2654afee4da87e30557ae04e93e
Gautier DAVID <gautier.d.91@gmail.com>
2022-08-03 15:02:44 +0200

    all test for the fourth exercises are cleared

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(arr, ...args) {
+  
+    for(const arg of args) {
+        if(arr.includes(arg)) arr.splice(arr.indexOf(arg), 1)
+    }
+    
+    return arr
+  };
 
 // Do not edit below this line
 module.exports = removeFromArray;
