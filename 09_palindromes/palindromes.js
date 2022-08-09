abf303a4512e1c9ab63c090ba9b6b542b5777a51
Gautier DAVID <gautier.d.91@gmail.com>
2022-08-09 15:14:16 +0200

    pass all palindrome tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let regex = /[A-Za-z]/g;
+    return str.toLowerCase().match(regex).reverse().join('') === str.toLowerCase().match(regex).join('');
 };
 
 // Do not edit below this line
