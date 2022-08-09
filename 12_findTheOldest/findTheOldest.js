1efd3113f0cd8317fd7701270d3820892ea9c8b5
Gautier DAVID <gautier.d.91@gmail.com>
2022-08-09 18:10:06 +0200

    find the oldest person from an array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let result = [];
+    let age = 0;
+    
+    people.map(person => {
 
-};
+        if(!person.hasOwnProperty("yearOfDeath")) person.yearOfDeath = new Date().getFullYear();
+
+        if(person.yearOfDeath - person.yearOfBirth > age) {
+            age = person.yearOfDeath - person.yearOfBirth;
+            result = person;
+        }
+    });
+    
+    return result;
+  
+  };
 
 // Do not edit below this line
 module.exports = findTheOldest;
