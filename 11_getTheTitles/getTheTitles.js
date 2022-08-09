8b1d64307ce788bee30e2237007a17696bc44c2e
Gautier DAVID <gautier.d.91@gmail.com>
2022-08-09 16:26:05 +0200

    get titles from an array

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,11 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function(obj) {
+    let result = [];
+    for(let i = 0; i < obj.length; i++) {
+        result.push(obj[i]["title"])
+    }
+    return result
+  };
+  
 
 // Do not edit below this line
 module.exports = getTheTitles;
