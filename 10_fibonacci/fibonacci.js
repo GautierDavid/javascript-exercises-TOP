5633c7dfd8b02930380b653f7e5d21306eeb816f
Gautier DAVID <gautier.d.91@gmail.com>
2022-08-09 15:46:01 +0200

    give the nth number of the fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if(n < 1) return "OOPS"
 
-};
+    if(typeof(n) === "string") n = Number(n)
+
+    const fib = [1, 1];
+
+    for (let i = 0; fib.length < n; i++) {
+      fib.push(fib[i] + fib[i + 1])
+    }
+    
+    return fib.at(-1)
+  };
 
 // Do not edit below this line
 module.exports = fibonacci;
